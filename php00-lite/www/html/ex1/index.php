2c3668a25bc09bf58c68b80d93e3c48e9b6f8af4
Takayuki Kinoshita <takayukisoccer0527@icloud.com>
2022-02-08 17:56:07 +0900

    second

--- php00-lite/www/html/ex1/index.php
+++ php00-lite/www/html/ex1/index.php
@@ -1,4 +1,20 @@
 <?php
   date_default_timezone_set('Asia/Tokyo');
   echo date("Y/m/t") . "\n";
+
+  //配列を使用し、要素順に(日:0〜土:6)を設定する
+$week = [
+  '日', //0
+  '月', //1
+  '火', //2
+  '水', //3
+  '木', //4
+  '金', //5
+  '土', //6
+];
+ 
+$date = date('w');
+ 
+//日本語で曜日を出力
+echo $week[$date] . '曜日';
   
